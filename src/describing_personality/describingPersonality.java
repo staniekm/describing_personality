2d2d7140de2f3f1c1b601c13c8b120d83bee3e61
„staniekm” <marcin.staniek97@gmail.comssh-keygen -t rsa -C marcin.staniek97@gmail.com>
2019-05-12 15:49:16 +0200

    v1.1

--- src/describing_personality/describingPersonality.java
+++ src/describing_personality/describingPersonality.java
@@ -11,18 +11,86 @@ public class DescribingPersonality {
 		
 		String firstName = getStringDialog("Name", dialogTitle);
 		String lastName = getStringDialog("Last Name", dialogTitle);
+		String personality = description(descriptionNumber(addTwoStrings(firstName, lastName).toLowerCase()));
+
 		String nickname = getStringDialog("Nickname", dialogTitle);
-		String dateOfBirthday = getStringDialog("Birthday", dialogTitle);
-	
-		String personality = description(descriptionNumber(addTwoStrings(firstName, lastName).toLowerCase()));//getting descriptions
 		String perceivedPersonality = description(descriptionNumber(nickname.toLowerCase()));
-		String destiny = description(sumOfDigits(Integer.parseInt(dateOfBirthday)));
+
+		String dateOfBirthday = getStringDialog("Birthday", dialogTitle);
+		String destiny = description(sumOfDigits(Integer.parseInt(dateOfBirthday)));	
 		
 		outputDialog(finalDialog(personality, perceivedPersonality, destiny), dialogTitle);		
 	}
+
+	public static String getStringDialog(String info, String title) {
+		String string = JOptionPane.showInputDialog(null, info, title, JOptionPane.PLAIN_MESSAGE);
+		return string;
+	}
+	
+	public static void outputDialog(String content, String title) {
+		JOptionPane.showMessageDialog(null, content, title, JOptionPane.PLAIN_MESSAGE);
+	}
+	
+	public static String finalDialog(String personality, String perceivedPersonality, String destiny) {//create personal descriptions....
+		String finalDialog = "You're " + personality + "\n" +
+				"You're perceived as " + perceivedPersonality + "\n" +
+				"Your destiny is to be "+ destiny;
+		return finalDialog;
+	}
+
+	public static String description(int descriptionNumber) {//list of descriptions
+		
+		String[] description = new String[9];
+		description[0] = "powerfull, aggresive, self-centered, ambitious, quick-tempered, lonely";
+		description[1] = "passive, indecesive, shy, modest, charming, gentle";
+		description[2] = "creative creative, bossy, versatile, optimistic, talkative, independent";
+		description[3] = "solid, confident, steady, moody, practical, capable";
+		description[4] = "nervous, attractive, impatient, lively, adventurous, irresponsible";
+		description[5] = "loyal, affectionate, hard-working, friendly, balanced, self-satisfied";
+		description[6] = "talented, serious, pessimistic, self-controlled, proud, sensitive";
+		description[7] = "tought, efficent, materialistic, energeric, selfish, strong-minded";
+		description[8] = "idealistic, romantic, impulsive, rebelious, determined, insecure";
+				
+		return description[descriptionNumber-1];
+	}
+	
+	public static int descriptionNumber(String str) {//description number from string
+		int counter = 0;//counter for destiny number
+		
+		for (int i =0; i < str.length(); i++) { //letters to numbers
+			
+			char c = str.charAt(i); //char from string
+			
+			if(c == 'a' || c == 'i' || c == 'q' || c == 'j' || c == 'y') counter += 1;
+			if(c == 'b' || c == 'k' || c == 'r') counter += 2;
+			if(c == 'c' || c == 'g' || c == 'l' || c == 's') counter += 3;
+			if(c == 'd' || c == 'm' || c == 't') counter += 4;
+			if(c == 'e' || c == 'h' || c == 'n') counter += 5;
+			if(c == 'u' || c == 'v' || c == 'w' || c == 'x') counter += 6;
+			if(c == 'o' || c == 'z') counter += 7;
+			if(c == 'f' || c == 'p') counter += 8;
+		}
+		int sum = sumOfDigits(counter);
+		return sum;
+	}
+	
+	public static int sumOfDigits(int num) {
+        int sum = 0;
+        while (num > 0){
+            sum = sum + num % 10;
+            num = num / 10;
+        }
+        sum = (sum <10) ? sum : sumOfDigits(sum);
+        return sum;
+    }
+	
+	public static String addTwoStrings(String str1, String str2) {
+		String str = str1 + str2;
+		return str;
+	}
 	
 	public static void test() {
-	
+		
 		String nameTest = "Marcin";
 		String lastNameTest = "Staniek";
 		String nicknameTest = "Stanik";
@@ -31,7 +99,7 @@ public class DescribingPersonality {
 		System.out.println("Testing program...\n" + "Name: " + nameTest + "\nLast Name: " + lastNameTest + 
 				"\nNickname: " + nicknameTest + "\nBirthday: " + dateOfBirthdayTest);
 		
-		String personality = description(descriptionNumber(addTwoStrings(nameTest, lastNameTest).toLowerCase()));//getting descriptions
+		String personality = description(descriptionNumber(addTwoStrings(nameTest, lastNameTest).toLowerCase()));
 		String perceivedPersonality = description(descriptionNumber(nicknameTest.toLowerCase()));
 		String destiny = description(sumOfDigits(Integer.parseInt(dateOfBirthdayTest)));
 			
@@ -53,75 +121,5 @@ public class DescribingPersonality {
 		for(int i=1;i<10;i++)//printing all descriptions
 			System.out.println(i + ". " + description(i));
 	}
-
-	public static String getStringDialog(String info, String title) {
-		String string = JOptionPane.showInputDialog(null, info, title, JOptionPane.PLAIN_MESSAGE);
-		return string;
-	}
-	
-	public static void outputDialog(String content, String title) {
-		JOptionPane.showMessageDialog(null, content, title, JOptionPane.PLAIN_MESSAGE);
-	}
-	
-	public static String finalDialog(String personality, String perceivedPersonality, String destiny) {
-		String finalDialog = "You're " + personality + "\n" +
-				"You're perceived as " + perceivedPersonality + "\n" +
-				"Your destiny is to be "+ destiny;
-		return finalDialog;
-	}
-
-	public static String description(int descriptionNumber) {
-		
-		String[] description = new String[9];
-		description[0] = "powerfull, aggresive, self-centered, ambitious, quick-tempered, lonely";
-		description[1] = "passive, indecesive, shy, modest, charming, gentle";
-		description[2] = "creative creative, bossy, versatile, optimistic, talkative, independent";
-		description[3] = "solid, confident, steady, moody, practical, capable";
-		description[4] = "nervous, attractive, impatient, lively, adventurous, irresponsible";
-		description[5] = "loyal, affectionate, hard-working, friendly, balanced, self-satisfied";
-		description[6] = "talented, serious, pessimistic, self-controlled, proud, sensitive";
-		description[7] = "tought, efficent, materialistic, energeric, selfish, strong-minded";
-		description[8] = "idealistic, romantic, impulsive, rebelious, determined, insecure";
-				
-		return description[descriptionNumber-1];
-	}
-	
-	public static int descriptionNumber(String str) {//creating destinySum from string
-		int counter = 0;//counter for destiny number
-		
-		for (int i =0; i < str.length(); i++) { //converting letters to numbers
-			
-			char c = str.charAt(i); //making char from string
-			
-			//System.out.println(letter);//test for all letters
-
-			if(c == 'a' || c == 'i' || c == 'q' || c == 'j' || c == 'y') counter += 1;
-			if(c == 'b' || c == 'k' || c == 'r') counter += 2;
-			if(c == 'c' || c == 'g' || c == 'l' || c == 's') counter += 3;
-			if(c == 'd' || c == 'm' || c == 't') counter += 4;
-			if(c == 'e' || c == 'h' || c == 'n') counter += 5;
-			if(c == 'u' || c == 'v' || c == 'w' || c == 'x') counter += 6;
-			if(c == 'o' || c == 'z') counter += 7;
-			if(c == 'f' || c == 'p') counter += 8;
-		}
-		int sum = sumOfDigits(counter);
-		return sum;
-	}
-	
-	public static int sumOfDigits(int num) {//adding all numbers from int
-        int sum = 0;
-        while (num > 0){
-            sum = sum + num % 10;
-            num = num / 10;
-        }
-        sum = (sum <10) ? sum : sumOfDigits(sum);
-        return sum;
-    }
-	
-	public static String addTwoStrings(String str1, String str2) {
-		String str = str1 + str2;
-		return str;
-	}
-	
 }
 	
